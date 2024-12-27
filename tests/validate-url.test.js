29f2a752ce64dafdf4eb4ac6d717c465b95d0eb0
TheBookKnight <cadav001@umn.edu>
2024-12-27 17:02:56 -0600

    2 - rename test spec for validateUrl

--- tests/validate-url.test.js
+++ tests/validate-url.test.js
@@ -2,14 +2,14 @@ import { describe, it } from 'node:test';
 import assert from 'node:assert';
 import { validateUrl } from '../src/validate-url.js';
 
-describe('YouTube', () => {
-    it('validateUrl should return true for valid urls', () => {
+describe('validateUrl', () => {
+    it('should return true for valid urls', () => {
         assert(validateUrl('https://www.youtube.com'), true)
         assert(validateUrl('https://www.google.com'), true)
         assert(validateUrl('http://www.something.com'), true)
     });
 
-    it('validateUrl should return false for invalid urls', () => {
+    it('should return false for invalid urls', () => {
         assert.equal(validateUrl(''), false)
         assert.equal(validateUrl(123), false)
         assert.equal(validateUrl('url'), false)
