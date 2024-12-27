0d1a30caf7423f18b1e1146ebd08094d28b86c10
TheBookKnight <cadav001@umn.edu>
2024-12-27 03:58:30 -0600

    2 - add test for validateUrl

--- tests/youtube.test.js
+++ tests/youtube.test.js
@@ -3,9 +3,15 @@ import assert from 'node:assert';
 import { validateUrl } from '../src/validate-url.js';
 
 describe('YouTube', () => {
-    it('validateUrl should work', () => {
+    it('validateUrl should return true for valid urls', () => {
         assert(validateUrl('https://www.youtube.com'), true)
         assert(validateUrl('https://www.google.com'), true)
         assert(validateUrl('http://www.something.com'), true)
+    });
+
+    it('validateUrl should return false for invalid urls', () => {
+        assert.equal(validateUrl(''), false)
+        assert.equal(validateUrl(123), false)
+        assert.equal(validateUrl('url'), false)
     });
 });
